548e3d2c3b2b0ec8f61dcfb8fe4c7aca55461446
Maria Ferreira <116128906+MariaFerreira19@users.noreply.github.com>
2023-09-21 20:30:10 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -5,5 +5,11 @@ public class Produto {
 	int quantidade;
 	double preco;
 	//Definição de métodos da classe Produto
-	
+	public void mostraPreco()
+	{
+		System.out.println(preco);
+	}
+	public void atualizaPreco(double porc) {
+		preco = preco + preco * porc / 100;
+	}
 }
